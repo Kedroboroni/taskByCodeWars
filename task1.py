121ca62cc1edc558d0ddc98c0c3ce0b8ad2fe43e
kedroboroni <svishoyrik@mail.ru>
2024-08-28 23:58:55 +0300

    Завершите решение таким образом, чтобы оно удаляло весь текст, следующий за любым из переданных маркеров комментариев. Все пробелы в конце строки также должны быть удалены.

--- task1.py
+++ task1.py
@@ -1,6 +1,2 @@
 def strip_comments(string, markers):
     return "\n".join( [ str(min( [ str(  strings[:strings.find(mark)] ).rstrip() for mark in markers] ) ) for strings in string.splitlines( ) ] )
-
-
-l = strip_comments("apples, pears # and bananas\ngrapes\nbananas !apples", ["!", "#"])
-print(l)
